0efb166f6fefe95f8102ad478a387cc30bb155e4
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-03 21:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const form = document.querySelector("form");
+const btnSubmit = document.querySelector(".btn-submit");
+
+const table = document.querySelector(".table");
+const book_list = document.querySelector("#book-list");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new window.FormData(e.target);
+  [title, author, isbn] = [...formData];
+
+  addBook(title, author, isbn);
+});
+
+function addBook() {
+  const html = `
+    <tr>
+              <td>${title[1]}</td>
+              <td>${author[1]}</td>
+              <td>${isbn[1]}</td>
+              <td><button class="delete">&times;</button></td>
+    </tr>`;
+  book_list.insertAdjacentHTML("beforeend", html);
+
+  const deleteRow = document.querySelectorAll(".delete");
+  deleteRow.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      //   e.preventDefault();
+      console.log(e.target.parentElement.parentElement.remove());
+    });
+  });
+}
